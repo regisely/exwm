829c0c99630701aae7d0d6391b968fa828544451
Regis A. Ely <regisaely@gmail.com>
2021-05-01 13:23:28 -0300

    Workaround to keyboard focus in Chrome

--- exwm-layout.el
+++ exwm-layout.el
@@ -179,7 +179,7 @@
                          :window id :value-mask xcb:CW:EventMask
                          :event-mask (exwm--get-client-event-mask)))
       (exwm-layout--set-state id xcb:icccm:WM_STATE:IconicState)
-      (cl-pushnew xcb:Atom:_NET_WM_STATE_HIDDEN exwm--ewmh-state)
+      ;; (cl-pushnew xcb:Atom:_NET_WM_STATE_HIDDEN exwm--ewmh-state)
       (exwm-layout--set-ewmh-state id)
       (exwm-layout--auto-iconify)
       (xcb:flush exwm--connection))))
